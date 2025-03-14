0efa6183c65a1370f8c3acdb9921cec1cf4712d9
yedgesonam <yedgesonam@gmail.com>
2025-03-13 23:05:19 -0700

    Add backend code to the new branch

--- src/main/java/config/JwtProvider.java
+++ src/main/java/config/JwtProvider.java
@@ -6,7 +6,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 

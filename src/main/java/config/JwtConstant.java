4663aff3b2be27a1fafad39853a530c01434556c
yedgesonam <yedgesonam@gmail.com>
2025-03-13 23:22:47 -0700

    updated jwt info

--- src/main/java/config/JwtConstant.java
+++ src/main/java/config/JwtConstant.java
@@ -1,7 +1,7 @@
 package config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "hbctyvc8ubytyvbuybctv7yb7rv7e8uiniuytvcybucnijkcmgvctyucghiucbyvctuybc";
+    public static final String SECRET_KEY = "";
     public static final String JWT_HEADER = "Authorization";
 
 }
